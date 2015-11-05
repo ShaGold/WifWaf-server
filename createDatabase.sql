e3f0b2bf91730d08123f31f1eaef1972dc0abbd4
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-11-05 10:50:38 +0100

    Update script création BD

--- createDatabase.sql
+++ createDatabase.sql
@@ -12,5 +12,42 @@ CREATE TABLE User(
   birthday DATE not null,
   phoneNumber INT,
   description text,
-  photo VARCHAR(200)
+  photo VARCHAR(200),
+  flag INT
+);
+
+CREATE TABLE Dog(
+  idDog INT primary key autoincrement,
+  dogName VARCHAR(200) not null,
+  age INT,
+  breed VARCHAR(200),
+  size INT, --deux digit
+  getAlongWithMales VARCHAR(200),
+  getAlongWithFemales VARCHAR(200),
+  getAlongWithKids VARCHAR(200),
+  obedient INT,
+  description text,
+  sociable INT -- deux digit
+);
+
+CREATE TABLE DogBehaviour(
+  idDogBehaviour INT primary key autoincrement,
+  idDog INT,
+  idBehaviour INT
+);
+
+CREATE TABLE Behaviour(
+  idBehaviour INT primary key autoincrement,
+  description VARCHAR(200)
+);
+
+CREATE TABLE PhotoDog(
+  idPhotoDog INT primary key autoincrement,
+  idDog INT,
+  idPhoto INT
+);
+
+CREATE TABLE Photo(
+  idPhoto INT,
+  photo VARCHAR(250)
 );
