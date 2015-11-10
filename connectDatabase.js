8d19974c7c511df3e3cc3dcae8770416966bed86
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-11-10 22:30:02 +0100

    Correction format jsonArray

--- connectDatabase.js
+++ connectDatabase.js
@@ -22,5 +22,27 @@ function DBConnection(){
     if (err) throw err;
   });
 
+  this.addUser = function(user){
+    var req = "INSERT INTO USER(email, nickname, password, birthday, phoneNumber, description, photo) "
+                 + "VALUES('" + user.email + "', '" + user.nickname + "', '" + user.password + "', '" + user.birthday + "', '" + user.phoneNumber + "', '" + user.description + "', '" + user.photo + "');";
+     db.query(req, function select(err, result) {
+         if (err) {
+             console.log(err);
+             return;
+         }
+     });
+  }
+
+  this.addDog = function(dog){
+    var req = "INSERT INTO Dog(dogName, age, breed, size, getAlongWithMales, getAlongWithFemales, getAlongWithKids, getAlongWithHumans, description) "
+                 + "VALUES('" + dog.dogName + "', '" + dog.age + "', '" + dog.breed + "', '" + dog.size + "', '" + dog.getAlongWithMales + "', '" + dog.getAlongWithFemales + "', '" + dog.getAlongWithKids + "', '" + dog.getAlongWithHumans +  "', '" + dog.description + "');";
+     db.query(req, function select(err, result) {
+         if (err) {
+             console.log(err);
+             return;
+         }
+    });
+
+
   connection.end();
 }
