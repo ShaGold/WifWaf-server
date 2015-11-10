6e7888a63086189e66a632ca11afbe18c24e54eb
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-11-10 22:23:48 +0100

    Test JsonArray

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ io.sockets.on('connection', function (socket) {
     console.log('Un nouveau client est connecté !');
     socket.emit('onTestString', 'test');
     socket.emit('onTestJson', {"cheval":"valcheval","canard":"valcanard","chien":"valchien","chat":"valchat"});
+    socket.emit('onTestJsonArray', {"prénom":"Jimmy", "nomDeFamille":"Lopez"}, {"prénom":"Michel", "nomDeFamille":"Dupond"}, {"prénom":"Christophe", "nomDeFamille":"Dony"});
 
     socket.user = 0;
 
