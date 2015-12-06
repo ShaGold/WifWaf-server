a7334d0c19fbd81964d787f2254e6f3f2475e833
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-06 12:04:51 +0100

    Décomposition callbacks

--- connectDatabase.js
+++ connectDatabase.js
@@ -33,14 +33,18 @@ function DBConnection(){
              socket.emit("RTrySignUp", err['errno']);
          }
          else{
-             //on récupère l'id user pour le renvoyer au client
-                db.query("SELECT * FROM User Where email = " + user.email + ";", function(err, rows, fields) {
-                    var currentUser = new User(rows[0].idUser, rows[0].email, rows[0].nickname, rows[0].password, rows[0].birthday, rows[0].phoneNumber, rows[0].description, rows[0].photo);
-                    socket.emit("RTrySignUp", currentUser.idUser);
-                });
+             this.getUserByEmail(user.email, socket);
          }
      });
   };
+
+    this.getUserByEmail = function(email, socket){
+      //on récupère l'id user pour le renvoyer au client
+         db.query("SELECT * FROM User Where email = " + user.email + ";", function(err, rows, fields) {
+             //var currentUser = new User(rows[0].idUser, rows[0].email, rows[0].nickname, rows[0].password, rows[0].birthday, rows[0].phoneNumber, rows[0].description, rows[0].photo);
+             socket.emit("RTrySignUp", rows[0].idUser);
+         });
+    };
 
   this.addDog = function(dog){
       var req = "INSERT INTO Dog(dogName, idUser, age, breed, size, getAlongWithMales, getAlongWithFemales, getAlongWithKids, getAlongWithHumans, description) "
