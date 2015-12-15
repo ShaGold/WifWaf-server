c06fcfdfddc84924872d7a9fac34f919f1866260
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-15 23:03:09 +0100

    Ajout class Location

---
+++ controllers/Location.js
@@ -0,0 +1,8 @@
+module.exports.locations = Locations;
+
+function Locations(idLocation, latt, longitude, o){
+        this.idLocation = idLocation;
+        this.lattitude = latt;
+        this.longitude = longitude;
+        this.ordering = o;
+}
