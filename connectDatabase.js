e71a5b157a19740d33a7ed18aa2ea7858a413e42
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-15 23:44:17 +0100

    Test

--- connectDatabase.js
+++ connectDatabase.js
@@ -109,6 +109,7 @@ function DBConnection(){
   this.addLocation = function(location, socket){
       var req = "INSERT INTO Location(idWalk, lattitude, longitude, ordering) "
                  + "VALUES(LAST_INSERT_ID(), " + location.lattitude + "', '" + location.longitude + "', '" + location.ordering + ");";
+                 console.log(req);
       db.query(req, function select(err, result) {
          if (err) {
              console.log(err);
