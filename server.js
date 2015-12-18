acc2fecbcd38ea43b5a34ea8c50d2aa3897767e6
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-18 12:51:05 +0100

    Ajout log pour debug

--- server.js
+++ server.js
@@ -94,6 +94,7 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('TryAddWalk', function(walk){
         console.log('Tentative insertion de balade', walk);
+        console.log("Les location dans TryAddWalk ", walk.location);
         var newWalk = new Walk(0, walk.idUser, walk.walkName, walk.description, walk.city, walk.departure);
         db.addWalk(newWalk, walk.dogs, walk.location, socket);
     });
