12ff13cfa8d5a3a7fed0024c91fba9dd36469b12
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-26 00:50:28 +0100

    Test ajout

--- connectDatabase.js
+++ connectDatabase.js
@@ -273,8 +273,9 @@ function DBConnection(){
           else{
               var j;
               if (resultBeh != null){
-                  console.log("a");
+                  console.log("resultBeh");
                   for (j in resultBeh){
+                      console.log(j);
                       var req = "SELECT * FROM Behaviour WHERE Behaviour.idBehaviour = " + resultBeh[j].idBehaviour + ";";
                       db.query(req, function select(err, resultBehaviour) {
                           console.log("Result behaviour donne" + util.inspect(resultBehaviour));
