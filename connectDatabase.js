c96b9a58bacf3a97f7fcced99c094de9309bf9db
MarleneGuillemette <marlene.guillemette.etud@gmail.com>
2015-12-29 21:22:57 +0100

    Test

--- connectDatabase.js
+++ connectDatabase.js
@@ -291,22 +291,24 @@ this.recupPhoto = function(event, result, i, socket){
           else{
               var j;
               if (resultBeh.length > 0){
-                  var req = "SELECT * FROM Behaviour WHERE Behaviour.idBehaviour = " + resultBeh[j].idBehaviour + ";";
-                  db.query(req, function select(err, resultBehaviour) {
-                      if (err) {
-                          console.log(err);
-                          socket.emit(event, err['errno']);
-                      }
-                      else{
-                          result[i]['behaviours'] = resultBehaviour;
-                          if (i == result[i]['behaviours'].length - 1){
-                              //dernier element
-                              console.log("là où je passe", result[i].behaviours);
-                              socket.emit(event, result);
-                              console.log("ce qui est envoyé du serv", result);
+                  for (j in resultBeh){
+                      var req = "SELECT * FROM Behaviour WHERE Behaviour.idBehaviour = " + resultBeh[j].idBehaviour + ";";
+                      db.query(req, function select(err, resultBehaviour) {
+                          if (err) {
+                              console.log(err);
+                              socket.emit(event, err['errno']);
                           }
-                      }
-                  });
+                          else{
+                              result[i]['behaviours'] = resultBehaviour;
+                              if (i == result[i]['behaviours'] - 1){
+                                  //dernier element
+                                  console.log("là où je passe", result[i].behaviours);
+                                  socket.emit(event, result);
+                                  console.log("ce qui est envoyé du serv", result);
+                              }
+                          }
+                      });
+                  }
               }
               else{
                   socket.emit(event, result);
